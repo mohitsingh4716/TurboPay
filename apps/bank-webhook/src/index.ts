6dd438582a934b3b6eef790fc38148ea141c01d7
mohitsingh4716 <mohitsingh4716@gmail.com>
2025-02-22 17:49:22 +0530

    ValidationToRequest

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,22 +1,55 @@
 import express from "express";
 import db from "@repo/db/client";
 const app = express();
+import {z} from "zod";
 
 app.use(express.json())
 
+ const webhookSchema= z.object({
+    token:z.string(),
+    user_identifier:z.string(),
+    amount:z.number()
+})
+
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
+
+    const {success} = webhookSchema.safeParse(req.body);
+
+    if(!success){
+        res.status(411).json({
+            message:"Invalid Input Request"
+        })
+    }
+
+    const {token, user_identifier, amount}= req.body;
+
+    const actualAmount= amount*100;
+
+    const onRampTxn= await db.onRampTransaction.findFirst({
+        where:{
+            token
+        }
+    })
+    console.log(onRampTxn);
+
+     //Complete: check if this onRampTxn is proceesing or not
+    if(onRampTxn?.status !== "Processing"){
+        res.status(411).json({
+            message:"Transaction already processed"
+        });
+    }
+
+
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    // TODO: check if this onRampTxn is proceesing or not
-    
+
     const paymentInformation: {
         token: string;
         userId: string;
-        amount: string;
+        amount: number;
     } = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
+        token:token,
+        userId:user_identifier,
+        amount:actualAmount
     };
 
     try {
