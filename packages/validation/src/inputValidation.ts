9c099a4e02d238155abd5816871fd776015e022c
mohitsingh4716 <mohitsingh4716@gmail.com>
2025-04-01 14:56:06 +0530

    zod fixed

--- packages/validation/src/inputValidation.ts
+++ packages/validation/src/inputValidation.ts
@@ -1,4 +1,4 @@
-import z, { string } from 'zod';
+import { z } from 'zod';
 export const signupInput = z.object({
     name:z.string().min(3),
     phone:z.string().length(10),
@@ -10,10 +10,6 @@ export const signinInput = z.object({
     password:z.string().min(8)
 })
 
-export const forgetPasswordInput = z.object({
-    password:string().min(8)
-})
 
 export type SignupInputTypes = z.infer<typeof signupInput>;
 export type SigninInputTypes = z.infer<typeof signinInput> ;
-export type ForgetPasswordInput = z.infer<typeof forgetPasswordInput>;
