87581c226499de848552d9d99a67a36805a0e640
mohitsingh4716 <mohitsingh4716@gmail.com>
2025-04-03 16:31:20 +0530

    fixed

--- apps/user_Pay/app/(dashboard)/transfer/_components/add-money.tsx
+++ apps/user_Pay/app/(dashboard)/transfer/_components/add-money.tsx
@@ -42,7 +42,7 @@ const AddMoney = () => {
   }
   try {
       const {token} = await createOnRampTransaction(amount*100, provider);
-      const safeRedirectUrl = redirectUrl || "http://localhost:3000/dashboard";
+      const safeRedirectUrl = redirectUrl || "https://turbo-pay-two.vercel.app/dashboard";
 
 
     if (typeof token !== 'string') {
